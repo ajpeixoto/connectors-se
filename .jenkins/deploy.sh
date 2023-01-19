160bc6c4a2a630922db3a13fe754694512c2c17b
acatoire <acatoire@talend.com>
2023-01-19 14:34:15 +0100

    chore(TDI-48419) Snapshot build Test deploy:deploy

--- .jenkins/deploy.sh
+++ .jenkins/deploy.sh
@@ -3,12 +3,12 @@
 set -xe
 
 # Deploys on Nexus
-# Maven phases validate to install are skipped with DEPLOY profile in pom
+# deploy:deploy: https://maven.apache.org/guides/introduction/introduction-to-the-lifecycle.html#setting-up-your-project-to-use-the-build-lifecycle
 # $@: the extra parameters to be used in the maven command
 main() {
   local extraBuildParams=("$@")
 
-  mvn deploy \
+  mvn deploy:deploy \
     --errors \
     --batch-mode \
     --activate-profiles 'DEPLOY' \
