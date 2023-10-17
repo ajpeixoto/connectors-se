2f7b25f22abf767aee7ff78d2a649d7e63df7c12
Mykhailo Hryb <mykhailo.hryb@globallogic.com>
2023-10-17 17:03:21 +0300

    feat(TDI-50408): add support of application default credentials

--- google-storage/src/test/java/org/talend/components/google/storage/service/CredentialServiceTest.java
+++ google-storage/src/test/java/org/talend/components/google/storage/service/CredentialServiceTest.java
@@ -25,12 +25,14 @@ import org.junit.jupiter.api.Test;
 import org.talend.components.google.storage.datastore.AuthType;
 import org.talend.components.google.storage.datastore.GSDataStore;
 
+import static uk.org.webcompere.systemstubs.SystemStubs.withEnvironmentVariable;
+
 class CredentialServiceTest {
+
+    final URL resource = Thread.currentThread().getContextClassLoader().getResource("./engineering-test.json");
 
     @Test
     void newStorage() throws IOException {
-        final URL resource = Thread.currentThread().getContextClassLoader().getResource("./engineering-test.json");
-
         final File fic = new File(resource.getPath());
         final String json = new String(Files.readAllBytes(fic.toPath()));
         GSDataStore connection = new GSDataStore();
@@ -45,15 +47,19 @@ class CredentialServiceTest {
     }
 
     @Test
-    void testApplicationDefaultCredentials() {
-        GSDataStore connection = new GSDataStore();
-        connection.setAuthType(AuthType.APPLICATION_DEFAULT_CREDENTIALS);
+    void testApplicationDefaultCredentials() throws Exception {
+        withEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", resource.getPath())
+                .execute(() -> {
+                    GSDataStore connection = new GSDataStore();
+                    connection.setAuthType(AuthType.APPLICATION_DEFAULT_CREDENTIALS);
 
-        final CredentialService credentialService = new CredentialService();
-        final GoogleCredentials credentials = credentialService.getCredentials(connection);
-        Assertions.assertNotNull(credentials);
+                    final CredentialService credentialService = new CredentialService();
+                    final GoogleCredentials credentials = credentialService.getCredentials(connection);
+                    Assertions.assertNotNull(credentials);
 
-        final Storage storage = credentialService.newStorage(credentials);
-        Assertions.assertNotNull(storage);
+                    final Storage storage = credentialService.newStorage(credentials);
+                    Assertions.assertNotNull(storage);
+                });
+
     }
 }
